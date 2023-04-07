378f58bd516e60fbf43f712634414b3444f9bd64
Jesse Knaapen <Jesse.Knaapen@gmail.com>
2023-04-07 09:35:38 +0200

    fixed enemy hitbox don't work right bug

--- src/enemy.cpp
+++ src/enemy.cpp
@@ -6,8 +6,11 @@ void Enemy::checkPlayerKill() {
     // I use the same one as the ball one but with some little tweaks
     Player player;
     for (int i = 0; i < enemyCount; ++i) {
-        if (playerPosX <= enemyPosX[i] + 30 && playerPosX >= enemyPosX[i] - 30 &&
-            playerPosY <= enemyPosY[i] + 15 && playerPosY >= enemyPosY[i] - 15) {
+        // if (playerPosX <= enemyPosX[i] + 30 && playerPosX >= enemyPosX[i] - 30 &&
+        //      playerPosY <= enemyPosY[i] + 15 && playerPosY >= enemyPosY[i] - 15) {
+
+        if (playerPosX >= enemyPosX[i] && playerPosX <= enemyPosX[i] + 60 &&
+            playerPosY >= enemyPosY[i] && playerPosY <= enemyPosY[i] + 30) {
             enemyPosX[i] = -100;
             enemyPosY[i] = -100;
             player.kill();
